1a762e88d2a69cbf84aa85401257902fc0062910
Vince Fulco <vfulco@weisisheng.cn>
2021-08-05 17:02:55 +0800

    trying to pass ahard coded dist directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 
 echo "Change directory to Source"
-cd website
+cd ${DIST_DIR}
 # doesnt work
 # cd ./build/${PROJECT_NAME}
 #cd ${PROJECT_NAME}
